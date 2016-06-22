f947f6bac473801ef6b5672f77b9e510b25ff828
Kateryna Senchenko <kateryna.senchenko@gmail.com>
2016-06-22 14:54:40 +0300

    Added test for empty input

---
+++ src/test/java/com/javaclasses/calculator/MathCalculatorImplTest.java
@@ -0,0 +1,33 @@
+package com.javaclasses.calculator;
+
+import com.javaclasses.calculator.impl.MathCalculatorImpl;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class MathCalculatorImplTest {
+
+
+    private final MathCalculator calculator = new MathCalculatorImpl();
+
+    @Test
+    public void testEmptyInput(){
+
+        try {
+            calculator.evaluateMathExpression("");
+            fail("Expected exception was not thrown");
+        } catch (Exception e) {
+            assertEquals("Input should contain mathematical expression", e.getMessage());
+        }
+
+        try {
+            calculator.evaluateMathExpression(" ");
+            fail("Expected exception was not thrown");
+        } catch (Exception e) {
+            assertEquals("Input should contain mathematical expression", e.getMessage());
+        }
+    }
+
+
+}
