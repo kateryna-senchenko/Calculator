75eebe8dd53caf968cda85e2708c0e8a833ee4ea
Kateryna Senchenko <kateryna.senchenko@gmail.com>
2016-06-22 12:44:15 +0300

    Initial project configuration

---
+++ src/main/java/com/javaclasses/calculator/Main.java
@@ -0,0 +1,9 @@
+package com.javaclasses.calculator;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/javaclasses/calculator/MainTest.java
@@ -0,0 +1,6 @@
+package com.javaclasses.calculator;
+
+
+public class MainTest {
+
+}
