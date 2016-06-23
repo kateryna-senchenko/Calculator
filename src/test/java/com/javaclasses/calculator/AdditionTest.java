581d3e6b587834e8010fc2e4c58751c78bb2c27b
Kateryna Senchenko <kateryna.senchenko@gmail.com>
2016-06-23 15:20:08 +0300

    Added test for sequential addition

--- src/test/java/com/javaclasses/calculator/AdditionTest.java
+++ src/test/java/com/javaclasses/calculator/AdditionTest.java
@@ -25,4 +25,13 @@ public class AdditionTest {
 
         assertEquals("The result is incorrect", 5.2468, calculator.evaluateMathExpression("3.1234+2.1234"), 0.0);
     }
+
+    @Test
+    public void testSequentialAddition() throws EvaluationException {
+
+        final MathCalculator calculator = new MathCalculatorImpl();
+
+        assertEquals("The result is incorrect", 7.55, calculator.evaluateMathExpression("2.3+0.25+5"), 0.0);
+
+    }
 }
